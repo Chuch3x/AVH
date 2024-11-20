9ccfd7d5911eafa2e0ca57c2d549032c90d732e0
Chuch3x <c.augusto_mendez@hotmail.com>
2024-11-20 13:24:11 -0400

    9

--- commit_ccn/main.py
+++ commit_ccn/main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from commit_ccn.infraestructure.FlaskCommitMetricsController import app as application
 
 def main(request=None):
